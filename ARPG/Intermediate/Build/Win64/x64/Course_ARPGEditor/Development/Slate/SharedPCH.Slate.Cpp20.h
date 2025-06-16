4ff0258efdf1ecf73b1afeaaa9a4d842b40faab5
TomasAndrijevskis <tomassandrijevskis@gmail.com>
2025-06-16 18:31:42 +0300

    Make vs build files

--- ARPG/Intermediate/Build/Win64/x64/Course_ARPGEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
+++ ARPG/Intermediate/Build/Win64/x64/Course_ARPGEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
@@ -1,3 +1,3 @@
 // PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "D:/projects/Course_ARPG_5_4 - Copy/Intermediate/Build/Win64/x64/Course_ARPGEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
+#include "D:/projects/GIT HUB/ARPG/ARPG/Intermediate/Build/Win64/x64/Course_ARPGEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
 #include "Runtime/Slate/Public/SlateSharedPCH.h"
